37fb276d600b72469ed6a52436defdaf4b556000
dev_eao <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2025-06-13 11:49:33 +0100

    Add complaint_list view to show all submitted complaints

--- complaints/views.py
+++ complaints/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import ComplaintForm
+from .models import Complaint  # <-- Make sure this is here!
 
 def submit_complaint(request):
     if request.method == 'POST':
@@ -11,6 +12,9 @@ def submit_complaint(request):
         form = ComplaintForm()
     return render(request, 'complaints/submit_complaint.html', {'form': form})
 
-
 def complaint_submitted(request):
     return render(request, 'complaints/complaint_submitted.html')
+
+def complaint_list(request):
+    complaints = Complaint.objects.order_by('-created_at')
+    return render(request, 'complaints/complaint_list.html', {'complaints': complaints})
